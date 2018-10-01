e25ba2094be4423efd17a07e98739b4358957a41
Benjamin Greene <b.greene@wustl.edu>
2018-10-01 19:13:24 -0400

    added editing vc

---
+++ TestKitchen/EditingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EditingViewController.swift
+//  
+//
+//  Created by Ben Greene on 10/1/18.
+//
+
+import UIKit
+
+class EditingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //add cancel/save buttons programatically
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
